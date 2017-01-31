643a40b5baa4145b2636241b83b02c85c19056c2
Corinne Kelly <corinnemariekelly@gmail.com>
2017-01-31 16:03:49 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
 def square_array(array)
-  # your code here
-  # new_array = []
-  # array.each do |number|
-  #   new_array << number * number
-  # end
-  # new_array
-
-  array.collect do |number|
-    number*number
+  your code here
+  new_array = []
+  array.each do |number|
+    new_array << number * number
   end
+  new_array
+
+  # array.collect do |number|
+  #   number*number
+  # end
 end
